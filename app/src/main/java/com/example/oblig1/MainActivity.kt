e3f8f6706b25e5edf098709158b969a47947c0c5
Vilhelm <52938064+vvilhelmsen@users.noreply.github.com>
2026-02-04 07:11:41 +0100

    add ImagePickedActivity & link from MainActivity

--- app/src/main/java/com/example/oblig1/MainActivity.kt
+++ app/src/main/java/com/example/oblig1/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.oblig1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageViews = listOf(
+            R.id.grid1, R.id.grid2, R.id.grid3,
+            R.id.grid4, R.id.grid5, R.id.grid6,
+            R.id.grid7, R.id.grid8, R.id.grid9
+        )
+
+        imageViews.forEach { id ->
+            findViewById<ImageView>(id).setOnClickListener {
+                val intent = Intent(this, ImagePickedActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
